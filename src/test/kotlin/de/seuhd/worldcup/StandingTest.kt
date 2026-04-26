8feb1efe0571cf442e2c48d3fd5219d0f1b01285
LukasMoritz2707 <140209117+LukasMoritz2707@users.noreply.github.com>
2026-04-26 17:40:02 +0200

    Add Assignement 01 TestCase

--- src/test/kotlin/de/seuhd/worldcup/StandingTest.kt
+++ src/test/kotlin/de/seuhd/worldcup/StandingTest.kt
@@ -1,12 +1,29 @@
 package de.seuhd.worldcup
 
+import kotlinx.serialization.json.Json
+import java.io.File
 import kotlin.test.*
 
 class StandingsServiceTest {
 
     @Test
     fun `calculate standings for a simple group`() {
-        //TODO
-        assertEquals(0, 0)
+
+        val jsonFile = File("src/main/resources/world_cup_2026_full_data.json").readText(Charsets.UTF_8)
+        val worldCupData = Json.decodeFromString<WorldCupData>(jsonFile)
+
+        val group = worldCupData.groups
+        group[0].matches[0].homeScore = 4
+        group[0].matches[0].awayScore = 0
+
+        bets.add(Bet(group[0].matches[0].matchId, 1))
+
+        val testBets = calculateBettingScore(group)
+
+        val correctScore = testBets.correctBets
+        val correctTotal = testBets.totalBets
+
+        assertEquals(1, correctScore)
+        assertEquals(1, correctTotal)
     }
 }
